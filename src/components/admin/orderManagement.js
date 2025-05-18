97a41702ba4813680bbed110f308c7253e3bf1d6
Александр Колодько <aleks-webby@yandex.ru>
2025-05-18 17:24:56 +0300

    resolve

--- src/components/admin/orderManagement.js
+++ src/components/admin/orderManagement.js
@@ -128,12 +128,7 @@ const OrderManagement = () => {
                   <td>{item.product_name}</td>
                   <td>${parseFloat(item.price).toFixed(2)}</td>
                   <td>{item.quantity}</td>
-<<<<<<< HEAD
-                  <td>${(parseFloat(item.price * item.quantity)).toFixed(2)}</td>
-=======
-                  <td>${(parseFloat(item.price) * item.quantity).toFixed(2)}</td>
->>>>>>> f6a298b2211259316e11e834c4e6bae1c48d3b0c
-                </tr>
+                  <td>${(parseFloat(item.price) * item.quantity).toFixed(2)}</td></tr>
               ))}
               </tbody>
               <tfoot>
